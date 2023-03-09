1abd3e4e201699bc959265b07c4c1b1d69aab4f8
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2023-03-09 17:11:59 -0300

    adicionando camada application com servicos da api

---
+++ GithubRepositories.Application/Services/GithubService.cs
@@ -0,0 +1,15 @@
+﻿using GithubRepositories.Domain;
+
+namespace GithubRepositories.Application.Services
+{
+    public class GithubService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public GithubService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+       
+
+    }
+}
