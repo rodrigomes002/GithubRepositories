4deb1f224c39d401e9ee018803d314e16e07d968
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2023-03-09 17:11:25 -0300

    adicionando camada infra, configurando quartz, padrão repository, unitOfWork e context

---
+++ GithubRepositories.Infra/Context/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using GithubRepositories.Domain.Reps;
+using Microsoft.EntityFrameworkCore;
+
+namespace GithubRepositories.Infra.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PopularRep>? PopularRep { get; set; }
+    }
+}
---
+++ GithubRepositories.Infra/UoW/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using GithubRepositories.Domain;
+using GithubRepositories.Domain.Reps.Interface;
+using GithubRepositories.Infra.Context;
+using GithubRepositories.Infra.PopularReps;
+
+namespace GithubRepositories.Infra.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext applicationContext;
+        public IPopularRepRepository PopularRepRepository { get; }
+
+        public UnitOfWork(ApplicationContext applicationContext)
+        {
+            this.applicationContext = applicationContext;
+            this.PopularRepRepository = new PopularRepRepository(applicationContext);
+        }
+
+        public async Task CommitAsync()
+        {
+            await this.applicationContext.SaveChangesAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await this.applicationContext.DisposeAsync();
+        }
+    }
+}
